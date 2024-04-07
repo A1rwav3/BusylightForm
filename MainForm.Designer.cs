88aa33e1877e91764ece1b4976bb34ff887563a2
A1rwav3 <a1rwav3@gmail.com>
2024-04-07 23:56:15 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace BusylightForm
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.RedLightButton = new System.Windows.Forms.Button();
+            this.BlueLightButton = new System.Windows.Forms.Button();
+            this.NoLightButton = new System.Windows.Forms.Button();
+            this.reducedIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // RedLightButton
+            // 
+            this.RedLightButton.Location = new System.Drawing.Point(12, 12);
+            this.RedLightButton.Name = "RedLightButton";
+            this.RedLightButton.Size = new System.Drawing.Size(253, 58);
+            this.RedLightButton.TabIndex = 0;
+            this.RedLightButton.Text = "RED";
+            this.RedLightButton.UseVisualStyleBackColor = true;
+            this.RedLightButton.Click += new System.EventHandler(this.RedLightButton_Click);
+            // 
+            // BlueLightButton
+            // 
+            this.BlueLightButton.Location = new System.Drawing.Point(12, 76);
+            this.BlueLightButton.Name = "BlueLightButton";
+            this.BlueLightButton.Size = new System.Drawing.Size(253, 58);
+            this.BlueLightButton.TabIndex = 2;
+            this.BlueLightButton.Text = "BLUE";
+            this.BlueLightButton.UseVisualStyleBackColor = true;
+            this.BlueLightButton.Click += new System.EventHandler(this.BlueLightButton_Click);
+            // 
+            // NoLightButton
+            // 
+            this.NoLightButton.Location = new System.Drawing.Point(12, 141);
+            this.NoLightButton.Name = "NoLightButton";
+            this.NoLightButton.Size = new System.Drawing.Size(253, 58);
+            this.NoLightButton.TabIndex = 4;
+            this.NoLightButton.Text = "OFF";
+            this.NoLightButton.UseVisualStyleBackColor = true;
+            this.NoLightButton.Click += new System.EventHandler(this.NoLightButton_Click);
+            // 
+            // reducedIcon
+            // 
+            this.reducedIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("reducedIcon.Icon")));
+            this.reducedIcon.Text = "Busylight Controls";
+            this.reducedIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.reducedIcon_MouseDoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 211);
+            this.Controls.Add(this.NoLightButton);
+            this.Controls.Add(this.BlueLightButton);
+            this.Controls.Add(this.RedLightButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowInTaskbar = false;
+            this.Text = "Busylight Omega controls";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RedLightButton;
+        private System.Windows.Forms.Button BlueLightButton;
+        private System.Windows.Forms.Button NoLightButton;
+        private System.Windows.Forms.NotifyIcon reducedIcon;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,96 @@
+﻿namespace BusylightForm
+{
+    using Busylight;
+    using NonInvasiveKeyboardHookLibrary;
+    using System;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        private KuandoSDK sdk;
+        private KeyboardHookManager manager;
+        private ModifierKeys[] modifiers;
+
+        private int oneKey = 0x31;
+        private int twoKey = 0x32;
+        private int zeroKey = 0x30;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.modifiers = new ModifierKeys[] { NonInvasiveKeyboardHookLibrary.ModifierKeys.Control, NonInvasiveKeyboardHookLibrary.ModifierKeys.Shift };
+
+            this.sdk = new KuandoSDK("omega");
+            this.manager = new KeyboardHookManager();
+
+            this.manager.Start();
+
+            this.RegisterShortcuts();
+
+            this.HideForm();
+        }
+
+        private void RegisterShortcuts()
+        {
+            // Ctrl + shift + 0 to turn off
+            this.manager.RegisterHotkey(modifiers, zeroKey, () => LightOn(BusylightColor.Off));
+
+            // Ctrl + shift + 1 to turn red
+            this.manager.RegisterHotkey(modifiers, oneKey, () => LightOn(BusylightColor.Red));
+
+            // Ctrl + shift + 2 to turn blue
+            this.manager.RegisterHotkey(modifiers, twoKey, () => LightOn(BusylightColor.Blue));
+        }
+
+        private void RedLightButton_Click(object sender, EventArgs e)
+        {
+            this.LightOn(BusylightColor.Red);
+        }
+
+        private void BlueLightButton_Click(object sender, EventArgs e)
+        {
+            this.LightOn(BusylightColor.Blue);
+        }
+
+        private void NoLightButton_Click(object sender, EventArgs e)
+        {
+            this.LightOn(BusylightColor.Off);
+        }
+
+
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.HideForm();
+            }
+        }
+
+        private void reducedIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.ShowForm();
+        }
+
+        private void LightOn(BusylightColor color)
+        {
+            this.sdk.Light(color);
+        }
+
+        private void HideForm()
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+            reducedIcon.Visible = true;
+        }
+
+        private void ShowForm()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            reducedIcon.Visible = false;
+        }
+    }
+}
